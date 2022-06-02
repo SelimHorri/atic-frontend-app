a4b8d89d012f5c4d87880965cf06ae0e85fe3386
SelimHorri <horri.slim@gmail.com>
2022-06-02 11:12:36 +0100

    add details

--- src/app/component/saloon/saloon-detail/saloon-detail.component.ts
+++ src/app/component/saloon/saloon-detail/saloon-detail.component.ts
@@ -1,10 +1,15 @@
 
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { Category } from 'src/app/model/category';
+import { PageResponse } from 'src/app/model/response/page/page-response';
 import { Saloon } from 'src/app/model/saloon';
+import { ServiceDetail } from 'src/app/model/service-detail';
 import { ErrorHandlerService } from 'src/app/service/error-handler.service';
 import { SaloonService } from 'src/app/service/saloon.service';
+import { ServiceDetailService } from 'src/app/service/service-detail.service';
 
 @Component({
   selector: 'app-saloon-detail',
@@ -14,8 +19,10 @@ import { SaloonService } from 'src/app/service/saloon.service';
 export class SaloonDetailComponent implements OnInit {
   
   public saloon!: Saloon;
+  public serviceDetails!: PageResponse;
   
   constructor(private saloonService: SaloonService,
+    private serviceDetailService: ServiceDetailService,
     private activatedRoute: ActivatedRoute,
     private errorHandlerService: ErrorHandlerService) {}
   
@@ -29,6 +36,8 @@ export class SaloonDetailComponent implements OnInit {
         this.saloonService.findById(p?.id).subscribe({
           next: (saloonPayload: any) => {
             this.saloon = saloonPayload?.responseBody;
+            this.getAllServiceDetails(p?.id);
+            
           },
           error: (errorResponse: HttpErrorResponse) =>
               this.errorHandlerService.extractExceptionMsg(errorResponse)
@@ -37,6 +46,38 @@ export class SaloonDetailComponent implements OnInit {
       error: (errorResponse: HttpErrorResponse) =>
           this.errorHandlerService.extractExceptionMsg(errorResponse)
     });
+  }
+  
+  public getAllServiceDetails(saloonId: number): void {
+    this.serviceDetailService.findAllByCategorySaloonId(saloonId).subscribe({
+      next: (serviceDetailPayload: any) => {
+        this.serviceDetails = serviceDetailPayload?.responseBody;
+      },
+      error: (errorResponse: HttpErrorResponse) =>
+          this.errorHandlerService.extractExceptionMsg(errorResponse)
+    });
+  }
+  
+  public onOpenModal(action: string): void {
+    const button = document.createElement("button");
+    button.type = "button";
+    button.style.display = "none";
+    button.setAttribute("data-bs-toggle", "modal");
+    
+    if (action === "createReservation")
+      button.setAttribute("data-bs-target", "#createReservation");
+    
+    const mainContainer = document.getElementById("main-container");
+    mainContainer?.appendChild(button);
+    button.click();
+  }
+  
+  private onChooseServiceDetail(serviceDetailId: number): void {
+    this.onOpenModal("createReservation");
+  }
+  
+  public onCreateReservation(ngForm: NgForm): void {
+    
   }
   
   
