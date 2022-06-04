b6ab3f13d5ba504340eb53300226d4fe07ba8022
SelimHorri <horri.slim@gmail.com>
2022-06-04 13:30:59 +0100

    refactor

--- src/app/model/credential.ts
+++ src/app/model/credential.ts
@@ -6,7 +6,6 @@ export class Credential {
   constructor(
     public id: number,
     public username: string,
-    public password: string,
     public role: UserRoleBasedAuthority | string,
     public isEnabled: boolean,
     public isAccountNonExpired: boolean,
