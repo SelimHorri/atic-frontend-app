138568373c01080acc01715288ef6cdc0332770b
SelimHorri <horri.slim@gmail.com>
2022-06-12 20:32:04 +0100

    add required default dependencies to inject

--- src/app/component/workspace/worker/timesheet/timesheet-calendar/timesheet-calendar.component.ts
+++ src/app/component/workspace/worker/timesheet/timesheet-calendar/timesheet-calendar.component.ts
@@ -1,5 +1,9 @@
 
 import { Component, OnInit } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/angular';
+import { CalendarService } from 'src/app/service/calendar.service';
+import { CredentialService } from 'src/app/service/credential.service';
+import { ErrorHandlerService } from 'src/app/service/error-handler.service';
 
 @Component({
   selector: 'app-timesheet-calendar',
@@ -8,10 +12,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimesheetCalendarComponent implements OnInit {
   
-  constructor() {}
+  public accountUrl!: string;
+  public calendarOptions!: CalendarOptions;
+  
+  constructor(private credentialService: CredentialService,
+    private calendarService: CalendarService,
+    private errorHandlerService: ErrorHandlerService) {}
   
   ngOnInit(): void {
-    
+    this.accountUrl = this.credentialService.getUserRole(`${sessionStorage.getItem("userRole")}`);
   }
   
   
