0c226976fc2f2078cb17b3b856476d036a30f74f
SelimHorri <horri.slim@gmail.com>
2022-06-18 09:46:12 +0100

    impl worker get profile logic

--- src/app/component/workspace/worker/profile/profile.component.ts
+++ src/app/component/workspace/worker/profile/profile.component.ts
@@ -1,5 +1,11 @@
 
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { WorkerProfileResponse } from 'src/app/model/response/worker-profile-response';
+import { CredentialService } from 'src/app/service/credential.service';
+import { WorkerProfileService } from 'src/app/service/employee/worker/worker-profile.service';
+import { ErrorHandlerService } from 'src/app/service/error-handler.service';
 
 @Component({
   selector: 'app-worker-profile',
@@ -8,9 +14,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
   
-  constructor() {}
+  public accountUrl!: string;
+  public workerProfileResponse!: WorkerProfileResponse;
+  
+  constructor(private workerProfileService: WorkerProfileService,
+    private credentialService: CredentialService,
+    private errorHandlerService: ErrorHandlerService) {}
   
   ngOnInit(): void {
+    this.accountUrl = this.credentialService.getUserRole(`${sessionStorage.getItem("userRole")}`);
+    this.getProfile();
+  }
+  
+  public getProfile(): void {
+    this.workerProfileService.getProfile().subscribe({
+      next: (responsePayload: any) => this.workerProfileResponse = responsePayload?.responseBody,
+      error: (errorResponse: HttpErrorResponse) =>
+          this.errorHandlerService.extractExceptionMsg(errorResponse)
+    });
+  }
+  
+  public onProfileUpdate(ngForm: NgForm): void {
     
   }
   
--- src/app/service/employee/worker/worker-profile.service.ts
+++ src/app/service/employee/worker/worker-profile.service.ts
@@ -1,13 +1,39 @@
 
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import * as moment from 'moment';
+import { map, Observable } from 'rxjs';
+import { DateBackendFormat } from 'src/app/model/date-backend-format';
+import { Task } from 'src/app/model/task';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkerProfileService {
   
-  constructor() {
-    
+  private apiUrl: string = environment.API_URL;
+  
+  constructor(private http: HttpClient) {
+    this.apiUrl = `${this.apiUrl}/employees/workers/profile`;
+  }
+  
+  public getProfile(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}`, {
+      headers: {
+        UsernameAuth: `${sessionStorage.getItem(`username`)}`,
+        Authorization: `Bearer ${sessionStorage.getItem(`jwtToken`)}`,
+      }
+    }).pipe(map((res: any) => {
+      res.responseBody.worker.birthdate = moment(res?.responseBody?.worker?.birthdate, DateBackendFormat.LOCAL_DATE).toDate();
+      res.responseBody.worker.hiredate = moment(res?.responseBody?.worker?.hiredate, DateBackendFormat.LOCAL_DATE).toDate();
+      res.responseBody.tasks.content?.map((t: Task) => {
+        t.taskDate = moment(t?.taskDate, DateBackendFormat.LOCAL_DATE_TIME).toDate();
+        t.startDate = moment(t?.startDate, DateBackendFormat.LOCAL_DATE_TIME).toDate();
+        t.endDate = moment(t?.endDate, DateBackendFormat.LOCAL_DATE_TIME).toDate();
+      });
+      return res;
+    }));
   }
   
   
